a01f8a53d82ec87eb6a30273a8ec45bc94bdf6a5
Jacob Graham <jakegraham54@gmail.com>
2024-04-29 15:57:54 -0400

    Partially implemented the grocery item adapter class for the recycler view

--- app/src/main/java/com/jacobdamiangraham/groceryhelper/ui/GroceryItemAdapter.kt
+++ app/src/main/java/com/jacobdamiangraham/groceryhelper/ui/GroceryItemAdapter.kt
@@ -1,12 +1,44 @@
 package com.jacobdamiangraham.groceryhelper.ui
 
-//class GroceryItemAdapter(val context: Context, private val groceryItemList: List<GroceryItem>):
-//    RecyclerView.Adapter<GroceryItemAdapter.GroceryItemViewHolder>() {
-//
-//        private lateinit var groceryItem: GroceryItem
-//        private var arrayListGroceryItems: ArrayList<View>
-//
-//    inner class GroceryItemViewHolder(groceryItemView: View): RecyclerView.ViewHolder(groceryItemView) {
-//
-//    }
-//}
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.jacobdamiangraham.groceryhelper.R
+import com.jacobdamiangraham.groceryhelper.model.GroceryItem
+
+class GroceryItemAdapter(val context: Context, private val groceryItemList: List<GroceryItem>):
+    RecyclerView.Adapter<GroceryItemAdapter.GroceryItemViewHolder>() {
+
+        private lateinit var groceryItem: GroceryItem
+        private var arrayListGroceryItems: ArrayList<View>
+
+        init {
+            arrayListGroceryItems = ArrayList()
+        }
+
+    inner class GroceryItemViewHolder(groceryItemView: View): RecyclerView.ViewHolder(groceryItemView) {
+        val groceryItemName: TextView = groceryItemView.findViewById(R.id.groceryItemNameTextView)
+        val groceryItemAmount: TextView = groceryItemView.findViewById(R.id.groceryItemAmountTextView)
+        val groceryItemCost: TextView = groceryItemView.findViewById(R.id.itemCostTextView)
+        val groceryItemStoreName: TextView = groceryItemView.findViewById(R.id.storeNameTextView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroceryItemViewHolder {
+        val groceryItemInflater = LayoutInflater.from(parent.context)
+        val groceryItemView = groceryItemInflater.inflate(R.layout.groceryitem, parent, false)
+        arrayListGroceryItems.add(groceryItemView)
+
+        return GroceryItemViewHolder(groceryItemView)
+    }
+
+    override fun getItemCount(): Int {
+        return arrayListGroceryItems.size
+    }
+
+    override fun onBindViewHolder(holder: GroceryItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
