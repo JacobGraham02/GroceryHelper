8eb6d01b46876c06c5726fa113ca02215ab106f4
Jacob Graham <jakegraham54@gmail.com>
2024-04-29 14:45:02 -0400

    Added a fragment that will bring up a screen that will allow a user to add a new grocery item to their list

--- app/src/main/java/com/jacobdamiangraham/groceryhelper/ui/GroceryItemAdapter.kt
+++ app/src/main/java/com/jacobdamiangraham/groceryhelper/ui/GroceryItemAdapter.kt
@@ -1,17 +1,12 @@
 package com.jacobdamiangraham.groceryhelper.ui
 
-import android.content.Context
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-import com.jacobdamiangraham.groceryhelper.model.GroceryItem
-
-class GroceryItemAdapter(val context: Context, private val groceryItemList: List<GroceryItem>):
-    RecyclerView.Adapter<GroceryItemAdapter.GroceryItemViewHolder>() {
-
-        private lateinit var groceryItem: GroceryItem
-        private var arrayListGroceryItems: ArrayList<View>
-
-    inner class GroceryItemViewHolder(groceryItemView: View): RecyclerView.ViewHolder(groceryItemView) {
-
-    }
-}
+//class GroceryItemAdapter(val context: Context, private val groceryItemList: List<GroceryItem>):
+//    RecyclerView.Adapter<GroceryItemAdapter.GroceryItemViewHolder>() {
+//
+//        private lateinit var groceryItem: GroceryItem
+//        private var arrayListGroceryItems: ArrayList<View>
+//
+//    inner class GroceryItemViewHolder(groceryItemView: View): RecyclerView.ViewHolder(groceryItemView) {
+//
+//    }
+//}
