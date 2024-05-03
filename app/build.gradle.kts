94619b0ac3d447b8c028b77d89d206c582b3a677
Jacob Graham <jakegraham54@gmail.com>
2024-05-03 10:29:04 -0400

    Added an activity sign in view and layout xml file that supports log ins with an email and password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.1.0")
     implementation("androidx.credentials:credentials:1.3.0-alpha03")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0-alpha03")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
     implementation(libs.firebase.ui.auth)
 }
--- app/src/main/java/com/jacobdamiangraham/groceryhelper/ui/signin/SignInView.kt
+++ app/src/main/java/com/jacobdamiangraham/groceryhelper/ui/signin/SignInView.kt
@@ -1,18 +1,73 @@
 package com.jacobdamiangraham.groceryhelper.ui.signin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.credentials.CredentialManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.jacobdamiangraham.groceryhelper.MainActivity
 import com.jacobdamiangraham.groceryhelper.databinding.ActivitySigninBinding
 
 class SignInView : AppCompatActivity() {
 
     private lateinit var activitySigninBinding: ActivitySigninBinding
-    private lateinit var credentialManager: CredentialManager
+    private lateinit var firebaseAuthentication: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activitySigninBinding = ActivitySigninBinding.inflate(layoutInflater)
         setContentView(activitySigninBinding.root)
+
+        firebaseAuthentication = FirebaseAuth.getInstance()
+
+        activitySigninBinding.signInButton.setOnClickListener {
+            attemptLogin()
+        }
+
+        activitySigninBinding.clearEmailFieldButton.setOnClickListener {
+            activitySigninBinding.emailInputField.text.clear()
+        }
+
+        activitySigninBinding.clearPasswordFieldButton.setOnClickListener {
+            activitySigninBinding.passwordInputField.text.clear()
+        }
+    }
+
+    private fun attemptLogin() {
+        val email = activitySigninBinding.emailInputField.text.toString().trim()
+        val password = activitySigninBinding.passwordInputField.text.toString().trim()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(
+                this,
+                "Both the email and password field must not be empty",
+                Toast.LENGTH_LONG)
+                .show()
+            return
+        }
+
+        firebaseAuthentication.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {signInTask ->
+                if (signInTask.isSuccessful) {
+                    val firebaseUser = firebaseAuthentication.currentUser
+                    redirectToMainActivity(firebaseUser)
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Your user account could not be found. Please try again",
+                        Toast.LENGTH_LONG)
+                        .show()
+                    return@addOnCompleteListener
+                }
+            }
+    }
+
+    private fun redirectToMainActivity(firebaseUser: FirebaseUser?) {
+        if (firebaseUser != null) {
+            val mainActivityIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainActivityIntent)
+            finish()
+        }
     }
 }
