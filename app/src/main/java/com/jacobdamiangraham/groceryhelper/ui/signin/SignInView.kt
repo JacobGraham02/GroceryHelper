9f27d3b9ba2fccb0bbcabc12d263396cadb742b6
Jacob Graham <jakegraham54@gmail.com>
2024-05-03 14:34:29 -0400

    Added real-time validation rules to the sign in view, the same rules that the register view has

--- app/src/main/java/com/jacobdamiangraham/groceryhelper/ui/signin/SignInView.kt
+++ app/src/main/java/com/jacobdamiangraham/groceryhelper/ui/signin/SignInView.kt
@@ -2,12 +2,17 @@ package com.jacobdamiangraham.groceryhelper.ui.signin
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.jacobdamiangraham.groceryhelper.MainActivity
+import com.jacobdamiangraham.groceryhelper.R
 import com.jacobdamiangraham.groceryhelper.databinding.ActivitySigninBinding
+import com.jacobdamiangraham.groceryhelper.enums.InputType
 import com.jacobdamiangraham.groceryhelper.ui.register.RegisterView
 import com.jacobdamiangraham.groceryhelper.utils.ValidationUtil
 
@@ -37,6 +42,55 @@ class SignInView : AppCompatActivity() {
 
         activitySignInBinding.registerAccountButton.setOnClickListener {
             redirectToRegisterActivity()
+        }
+
+        activitySignInBinding.emailInputField.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                validate(InputType.EMAIL, s.toString())
+            }
+        })
+
+        activitySignInBinding.passwordInputField.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                validate(InputType.PASSWORD, s.toString())
+            }
+        })
+    }
+
+    private fun validate(inputType: InputType, value: String) {
+        when (inputType) {
+            InputType.EMAIL -> {
+                if (ValidationUtil.isValidEmailAddress(value)) {
+                    activitySignInBinding.emailInputField.setBackgroundResource(R.drawable.edit_text_valid)
+                    activitySignInBinding.emailInputTextView.text = getString(R.string.valid_email)
+                    activitySignInBinding.emailInputTextView.setTextColor(ContextCompat.getColor(this, R.color.green))
+                    activitySignInBinding.emailInputField.setTextColor(ContextCompat.getColor(this, R.color.green))
+                } else {
+                    activitySignInBinding.emailInputField.setBackgroundResource(R.drawable.edit_text_invalid)
+                    activitySignInBinding.emailInputTextView.text = getString(R.string.invalid_email)
+                    activitySignInBinding.emailInputTextView.setTextColor(ContextCompat.getColor(this, R.color.red))
+                    activitySignInBinding.emailInputField.setTextColor(ContextCompat.getColor(this, R.color.red))
+                }
+            }
+
+            InputType.PASSWORD -> {
+                if (ValidationUtil.isValidPassword(value)) {
+                    activitySignInBinding.passwordInputField.setBackgroundResource(R.drawable.edit_text_valid)
+                    activitySignInBinding.passwordInputTextView.text = getString(R.string.valid_password)
+                    activitySignInBinding.passwordInputTextView.setTextColor(ContextCompat.getColor(this, R.color.green))
+                    activitySignInBinding.passwordInputField.setTextColor(ContextCompat.getColor(this, R.color.green))
+                } else {
+                    activitySignInBinding.passwordInputField.setBackgroundResource(R.drawable.edit_text_invalid)
+                    activitySignInBinding.passwordInputTextView.text = getString(R.string.invalid_password)
+                    activitySignInBinding.passwordInputTextView.setTextColor(ContextCompat.getColor(this, R.color.red))
+                    activitySignInBinding.passwordInputField.setTextColor(ContextCompat.getColor(this, R.color.red))
+                }
+            }
+            InputType.CONFIRM_PASSWORD -> {}
         }
     }
 
